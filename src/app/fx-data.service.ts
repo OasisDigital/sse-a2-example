3345c1088d009857d982c5432f4da1deedb38c1b
Kyle Cordes <kyle.cordes@oasisdigital.com>
2016-12-03 16:27:17 -0600

    Share one server connection for N components

--- src/app/fx-data.service.ts
+++ src/app/fx-data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/share';
 
 import { FxQuote } from './fx-quote';
 import { Sse } from './sse';
@@ -9,6 +10,6 @@ export class FxDataService {
   fxData: Observable<FxQuote>;
 
   constructor(sse: Sse) {
-    this.fxData = sse.observe('//localhost:8005/lowfreq');
+    this.fxData = sse.observe('//localhost:8005/lowfreq').share();
   }
 }
