e1b584e2fac898b87845e96a02bf59f1f15d9153
Kyle Cordes <kyle.cordes@oasisdigital.com>
2016-12-11 21:46:44 -0600

    super rough chart

--- src/app/pair-history-chart/pair-history-chart.component.ts
+++ src/app/pair-history-chart/pair-history-chart.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
+
+import { FxQuote } from '../fx-quote';
+
+interface Point {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-pair-history-chart',
   templateUrl: './pair-history-chart.component.html'
 })
-export class PairHistoryChartComponent implements OnInit {
+export class PairHistoryChartComponent implements OnChanges {
   @Input() width: number;
   @Input() height: number;
-  @Input() quotes: any;
+  @Input() quotes: FxQuote[];
   @Input() pair: string;
 
-  ngOnInit() {
+  margin = 8;
+
+  points: Point[];
+
+  ngOnChanges() {
+    const quotes = this.quotes
+      .filter(q => q.timestamp > 0);
+    const bids: number[] = quotes.map(q => parseFloat(q.bid));
+    const timestamps: number[] = quotes.map(q => q.timestamp);
+
+    const minBid = Math.min(...bids);
+    const maxBid = Math.max(...bids);
+
+    const minTimestamp = Math.min(...timestamps);
+    const maxTimestamp = Math.max(...timestamps);
+
+    const scaleTimestamps = (this.width - 2 * this.margin) /
+      Math.max(1, maxTimestamp - minTimestamp);
+
+    const scaleBids = (this.height - 2 * this.margin) /
+      Math.max(0.000001, maxBid - minBid);
+
+    this.points = quotes
+      .map(q => ({
+        x: this.margin + (q.timestamp - minTimestamp) * scaleTimestamps,
+        y: this.margin + (parseFloat(q.bid) - minBid) * scaleBids
+      }));
   }
 
 }
